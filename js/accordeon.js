88c4a175b6033df80518cec3f74b7a8df3737fd2
Dmytro <dmitriytris@gmail.com>
2017-08-21 03:28:01 +0300

    no message

---
+++ js/accordeon.js
@@ -0,0 +1,41 @@
+;(function () { 
+
+var title = document.getElementsByClassName('contact_name'),
+    cont = document.getElementsByClassName('contact_card');
+
+for (var i = 0; i < title.length; i++) {
+    var curTitle = title[i];
+    title[i].addEventListener('click', function() {
+        var classes = this.getAttribute('class'),
+            newClasses = '',
+            classesArr = classes.split(' '),
+            newClassesArr = classes.split(' ');
+        for (var j = 0; j < classesArr.length; j++) {
+            if (classesArr[j] == 'active') {
+                classesArr.splice([j], 1);
+            }
+        }
+        if (classesArr.length === newClassesArr.length) {
+            classesArr.push('active');
+            newClasses = classesArr.join(' ');
+        } else {
+            newClasses = classesArr.join(' ');
+        }
+
+        for (var l = 0; l < title.length; l++) {
+            oldClasses = title[l].getAttribute('class');
+            oldClassesArr = oldClasses.split(' ');
+            for (var k = 0; k < oldClassesArr.length; k++) {
+                if (oldClassesArr[k] == 'active') {
+                    oldClassesArr.splice([k], 1);
+                }
+                oldClasses = oldClassesArr.join(' ');
+                title[l].setAttribute('class', oldClasses);
+
+            }
+        }
+        this.setAttribute('class', newClasses);
+    })
+}
+
+})();
